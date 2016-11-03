c365bf2349241c4c043f7ad6736fe2630df077b8
Uriel Miranda <urielnovoa@gmail.com>
2016-11-03 08:15:15 -0600

    add clase 3

--- exe1/app.js
+++ exe1/app.js
@@ -1,3 +1,35 @@
-/**
- * Created by urielmiranda on 31/10/16.
- */
+(function(){
+    'use strict';
+
+    angular
+        .module("myapp",[])
+        .controller("myCtrl", myCtrl)
+        .controller("otherCtrl", otherCtrl);
+
+
+    function myCtrl(){
+        var mc = this;
+
+        mc.name = "Nekro";
+        mc.devf = [
+            {name: "Nekro", role: "Sensei"},
+            {name: "Diego", role: "Sensei"},
+            {name: "Manuel",role: "Alumno"},
+            {name: "Uzzi", role: "Alumno"}
+        ]
+    }
+
+    function otherCtrl(){
+        var other = this;
+
+        other.saludo = function(){
+            alert("Hello Devf");
+        }
+    }
+
+
+
+})();
+
+
+
